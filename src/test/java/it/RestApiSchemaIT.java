33550d13a4c63df243d2e9895bb35922349a6de7
Micha Kops <micha.kops@gmail.com>
2018-08-29 22:18:38 +0200

    random ports..

--- src/test/java/it/RestApiSchemaIT.java
+++ src/test/java/it/RestApiSchemaIT.java
@@ -5,16 +5,27 @@ import io.github.robwin.swagger.test.SwaggerAssertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes= Application.class)
 public class RestApiSchemaIT {
+  @LocalServerPort
+  int port;
+
   @Test
   public void validateThatImplementationMatchesDocumentationSpecification() {
     String apiContract = RestApiSchemaIT.class.getResource("/swagger-contract.yaml").getPath();
-    SwaggerAssertions.assertThat("http://localhost:8080/v2/api-docs")
+    String swaggerSchemaUrl = String.format("http://localhost:%d/v2/api-docs", port);
+    try {
+      Thread.sleep(20000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    SwaggerAssertions.assertThat(swaggerSchemaUrl)
         .isEqualTo(apiContract);
   }
 }
